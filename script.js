b5b540f16ec22c3b2554dbef7e1d7a3e24624433
BrianR <briomero76@gmail.com>
2022-02-12 15:31:05 -0500

    url listener has been added

--- script.js
+++ script.js
@@ -51,3 +51,19 @@ template('about',function(){
 route('/','home')
 route('/about','about')
 
+function resolverRoutes(route){
+    try{
+        return routes[route]
+    }catch(e){
+        throw new Error(`Route ${route} not found`)
+    }
+}
+
+function router(evt){
+    let url = window.location.hash.slice(1) || '/'
+    let route = resolveRoute(url)
+    route()
+}
+
+window.addEventListener('load',router)
+window.addEventListener('hashchange',router)
